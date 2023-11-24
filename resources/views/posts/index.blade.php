342dffe6453144f56b48bb619919c5aa3cf2e89d
ktnr1120 <ktnr1120@gmail.com>
2023-11-25 00:30:14 +0900

    ログイン認証

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,19 +7,22 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body class="antialoased">
-        <h1>Blog Name</h1>
-        <a href="/posts/create/">create</a>
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
-                    <p class='body'>{{ $post->body }}</p>
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links()}}
-        </div>
+        <x-app-layout>
+            <h1>Blog Name</h1>
+            <a href="/posts/create/">create</a>
+            <div class='posts'>
+                @foreach ($posts as $post)
+                    <div class='post'>
+                        <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
+                        <p class='body'>{{ $post->body }}</p>
+                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                    </div>
+                @endforeach
+            </div>
+            <div class='paginate'>
+                {{ $posts->links()}}
+            </div>
+            <p>ログインユーザー：{{ Auth::user()->name }}</p>
+        </x-app-layout>
     </body>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,19 +8,21 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
-       <h1 class="title">
-           {{ $post->title }}
-       </h1>
-       <div class="content">
-           <div class="content_post">
-               <h3>本文</h3>
-               <p>{{ $post->body }}</p>
+        <x-app-layout>
+            <div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
+           <h1 class="title">
+               {{ $post->title }}
+           </h1>
+           <div class="content">
+               <div class="content_post">
+                   <h3>本文</h3>
+                   <p>{{ $post->body }}</p>
+               </div>
+            </div>
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+           <div class="footer">
+               <a href="/">戻る</a>
            </div>
-        </div>
-        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-       <div class="footer">
-           <a href="/">戻る</a>
-       </div>
+       </x-app-layout>
     </body>
 </html>
